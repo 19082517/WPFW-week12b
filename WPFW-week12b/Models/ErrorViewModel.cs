8a7a5031150a9ab70c2e06cd273a09d6e48cf1e0
Mehdi <19082517@student.hhs.nl>
2020-11-24 14:40:38 +0100

    created new project

---
+++ WPFW-week12b/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WPFW_week12b.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
